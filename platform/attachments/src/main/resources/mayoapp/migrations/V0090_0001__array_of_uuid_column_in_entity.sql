d46cb22310e0414cfe37db194fc341f1179dacb3
Vincent Velociter <vincent.velociter@gmail.com>
2013-10-03 18:10:37 +0200

    Better formatting

--- platform/attachments/src/main/resources/mayoapp/migrations/V0090_0001__array_of_uuid_column_in_entity.sql
+++ platform/attachments/src/main/resources/mayoapp/migrations/V0090_0001__array_of_uuid_column_in_entity.sql
@@ -4,8 +4,33 @@ ALTER TABLE page ADD COLUMN gallery uuid[];
 ALTER TABLE article ADD COLUMN gallery uuid[];
 ALTER TABLE collection ADD COLUMN gallery uuid[];
 
-/* migrate all attachments that are images and whose parent is in one of the tables above to the new gallery field */
-UPDATE product SET gallery=array(SELECT entity_id FROM attachment JOIN entity ON attachment.entity_id = entity.id WHERE entity.parent_id = product.entity_id AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff'));
-UPDATE page SET gallery=array(SELECT entity_id FROM attachment JOIN entity ON attachment.entity_id = entity.id WHERE entity.parent_id = page.entity_id AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff'));
-UPDATE article SET gallery=array(SELECT entity_id FROM attachment JOIN entity ON attachment.entity_id = entity.id WHERE entity.parent_id = article.entity_id AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff'));
-UPDATE collection SET gallery=array(SELECT entity_id FROM attachment JOIN entity ON attachment.entity_id = entity.id WHERE entity.parent_id = collection.entity_id AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff'));
+/* migrate all attachments that are images and whose parent is in one of the 
+tables above to the new gallery field */
+UPDATE product SET gallery=array(
+    SELECT entity_id 
+    FROM attachment 
+    JOIN entity ON attachment.entity_id = entity.id 
+    WHERE entity.parent_id = product.entity_id 
+    AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff')
+);
+UPDATE page SET gallery=array(
+    SELECT entity_id
+    FROM attachment
+    JOIN entity ON attachment.entity_id = entity.id
+    WHERE entity.parent_id = page.entity_id
+    AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff')
+);
+UPDATE article SET gallery=array(
+    SELECT entity_id
+    FROM attachment
+    JOIN entity ON attachment.entity_id = entity.id
+    WHERE entity.parent_id = article.entity_id
+    AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff')
+);
+UPDATE collection SET gallery=array(
+    SELECT entity_id
+    FROM attachment
+    JOIN entity ON attachment.entity_id = entity.id
+    WHERE entity.parent_id = collection.entity_id
+    AND lower(extension) IN ('jpg', 'jpeg', 'gif', 'bmp', 'png', 'tiff')
+);
